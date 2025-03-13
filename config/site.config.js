55ab10089ea87f4be1d3275912965f822b24d25c
kadu3dg <117568681+kadu3dg@users.noreply.github.com>
2025-03-13 10:45:52 -0300

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Meus arquivos/Imagens/Capturas de tela'],
+  protectedRoutes: ['/Meusarquivos/Imagens/Capturasdetela'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'kadurock15@hotmail.com',
